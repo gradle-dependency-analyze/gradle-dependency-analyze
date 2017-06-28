00a10433b62a46440d9eb0939f373ff7133196ba
Wesley Hartford <wesley@ze.com>
2017-06-28 13:05:30 -0700

    Add support for gradle task output cache

--- src/main/groovy/ca/cutterslade/gradle/analyze/AnalyzeDependenciesPlugin.groovy
+++ src/main/groovy/ca/cutterslade/gradle/analyze/AnalyzeDependenciesPlugin.groovy
@@ -4,45 +4,52 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.SourceSet
 
+import java.util.concurrent.ConcurrentHashMap
+
 class AnalyzeDependenciesPlugin implements Plugin<Project> {
   @Override
   void apply(final Project project) {
-    project.configurations.create('permitUnusedDeclared')
-    project.configurations.create('permitTestUnusedDeclared')
-    project.task('analyzeDependencies')
-    if (project.tasks['classes']) {
-      project.task(dependsOn: 'classes', type: AnalyzeDependenciesTask, 'analyzeClassesDependencies') {
-        require = [
-            project.configurations.compile,
-            project.configurations.findByName('compileOnly'),
-            project.configurations.findByName('provided')
-        ]
-        allowedToDeclare = [
-            project.configurations.permitUnusedDeclared
-        ]
-        def output = project.sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).output
-        classesDirs = output.hasProperty('classesDirs') ? output.classesDirs : [output.classesDir]
-      }
-      project.analyzeDependencies.dependsOn('analyzeClassesDependencies')
+    if (project.rootProject == project) {
+      project.rootProject.extensions.add(ProjectDependencyResolver.CACHE_NAME, new ConcurrentHashMap<>())
     }
-    if (project.tasks['testClasses']) {
-      project.task(dependsOn: 'testClasses', type: AnalyzeDependenciesTask, 'analyzeTestClassesDependencies') {
-        require = [
-            project.configurations.testCompile,
-            project.configurations.findByName('testCompileOnly')
-        ]
-        allowedToUse = [
-            project.configurations.compile,
-            project.configurations.findByName('provided')
-        ]
-        allowedToDeclare = [
-            project.configurations.permitTestUnusedDeclared
-        ]
-        def output = project.sourceSets.getByName(SourceSet.TEST_SOURCE_SET_NAME).output
-        classesDirs = output.hasProperty('classesDirs') ? output.classesDirs : [output.classesDir]
+    if (project.plugins.hasPlugin('java')) {
+      project.configurations.create('permitUnusedDeclared')
+      project.configurations.create('permitTestUnusedDeclared')
+      project.task('analyzeDependencies')
+      if (project.tasks['classes']) {
+        project.task(dependsOn: 'classes', type: AnalyzeDependenciesTask, 'analyzeClassesDependencies') {
+          require = [
+              project.configurations.compile,
+              project.configurations.findByName('compileOnly'),
+              project.configurations.findByName('provided')
+          ]
+          allowedToDeclare = [
+              project.configurations.permitUnusedDeclared
+          ]
+          def output = project.sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).output
+          classesDirs = output.hasProperty('classesDirs') ? output.classesDirs : project.files(output.classesDir)
+        }
+        project.analyzeDependencies.dependsOn('analyzeClassesDependencies')
       }
-      project.analyzeDependencies.dependsOn('analyzeTestClassesDependencies')
+      if (project.tasks['testClasses']) {
+        project.task(dependsOn: 'testClasses', type: AnalyzeDependenciesTask, 'analyzeTestClassesDependencies') {
+          require = [
+              project.configurations.testCompile,
+              project.configurations.findByName('testCompileOnly')
+          ]
+          allowedToUse = [
+              project.configurations.compile,
+              project.configurations.findByName('provided')
+          ]
+          allowedToDeclare = [
+              project.configurations.permitTestUnusedDeclared
+          ]
+          def output = project.sourceSets.getByName(SourceSet.TEST_SOURCE_SET_NAME).output
+          classesDirs = output.hasProperty('classesDirs') ? output.classesDirs : project.files(output.classesDir)
+        }
+        project.analyzeDependencies.dependsOn('analyzeTestClassesDependencies')
+      }
+      project.check.dependsOn('analyzeDependencies')
     }
-    project.check.dependsOn('analyzeDependencies')
   }
 }
--- src/main/groovy/ca/cutterslade/gradle/analyze/AnalyzeDependenciesTask.groovy
+++ src/main/groovy/ca/cutterslade/gradle/analyze/AnalyzeDependenciesTask.groovy
@@ -3,17 +3,35 @@ package ca.cutterslade.gradle.analyze
 import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalysis
 import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.file.FileCollection
+import org.gradle.api.specs.Spec
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
+import java.lang.reflect.Method
+
 class AnalyzeDependenciesTask extends DefaultTask {
-  boolean justWarn
-  List<Configuration> require
-  List<Configuration> allowedToUse
-  List<Configuration> allowedToDeclare
-  Iterable<File> classesDirs
+  @Input
+  boolean justWarn = false
+  List<Configuration> require = []
+  List<Configuration> allowedToUse = []
+  List<Configuration> allowedToDeclare = []
+  @InputFiles
+  FileCollection classesDirs = project.files()
+  @OutputFile
+  File outputFile = project.file("$project.buildDir/dependency-analyze/result")
+
+  AnalyzeDependenciesTask() {
+    def methods = outputs.class.getMethods().grep {Method m -> m.name == 'cacheIf'}
+    if (methods) {
+      outputs.cacheIf({true})
+    }
+  }
 
   void setClassesDir(File classesDir) {
-    this.classesDirs = [classesDir]
+    this.classesDirs = project.files(classesDir)
   }
 
   @TaskAction
@@ -21,7 +39,7 @@ class AnalyzeDependenciesTask extends DefaultTask {
     project.logger.info "Analyzing dependencies of $classesDirs for [require: $require, allowedToUse: $allowedToUse, " +
         "allowedToDeclare: $allowedToDeclare]"
     ProjectDependencyAnalysis analysis =
-        new ProjectDependencyResolver(project.logger, require, allowedToUse, allowedToDeclare, classesDirs)
+        new ProjectDependencyResolver(project, require, allowedToUse, allowedToDeclare, classesDirs)
             .analyzeDependencies()
     StringBuffer buffer = new StringBuffer()
     ['usedUndeclaredArtifacts', 'unusedDeclaredArtifacts'].each {section ->
@@ -33,6 +51,8 @@ class AnalyzeDependenciesTask extends DefaultTask {
         }
       }
     }
+    outputFile.parentFile.mkdirs()
+    outputFile.text = buffer.toString()
     if (buffer) {
       def message = "Dependency analysis found issues.\n$buffer"
       if (justWarn) {
@@ -42,5 +62,48 @@ class AnalyzeDependenciesTask extends DefaultTask {
         throw new DependencyAnalysisException(message)
       }
     }
+  }
+
+  @InputFiles
+  FileCollection getAllArtifacts() {
+    project.files({
+      def files = ProjectDependencyResolver.removeNulls(
+          ProjectDependencyResolver.removeNulls(require)
+              *.resolvedConfiguration
+              *.firstLevelModuleDependencies
+              *.allModuleArtifacts
+              *.file.flatten() as Set<File>
+      )
+      project.logger.info "All Artifact Files: $files"
+      files
+    })
+
+  }
+
+  @InputFiles
+  FileCollection getRequiredFiles() {
+    getFirstLevelFiles(require, 'required')
+  }
+
+  @InputFiles
+  FileCollection getAllowedToUseFiles() {
+    getFirstLevelFiles(allowedToUse, 'allowed to user')
+  }
+
+  @InputFiles
+  FileCollection getAllowedToDeclareFiles() {
+    getFirstLevelFiles(allowedToDeclare, 'allowed to declare')
+  }
+
+  private FileCollection getFirstLevelFiles(List<Configuration> configurations, String name) {
+    project.files({
+      def files = ProjectDependencyResolver.removeNulls(
+          ProjectDependencyResolver.getFirstLevelDependencies(
+              ProjectDependencyResolver.removeNulls(configurations)
+          )*.allModuleArtifacts*.file.flatten()
+      )
+      project.logger.info "First level $name files: $files"
+      files
+    })
   }
 }
